e6eb0730ab7239feb301d89184d3f9f89e8da754
rajendrarajaramv <rajendrarajaramv@gmail.com>
2016-10-16 20:07:32 +0530

    initial commit

---
+++ higus_particle/src/main/java/com/rajendraverma/higus_particle/Application.java
@@ -0,0 +1,13 @@
+package com.rajendraverma.higus_particle;
+
+/**
+ * Created by rajendraverma on 16-10-2016.
+ */
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
